2f36be28fdb089931a91668fa743c05a9bdfaaed
Jonasz Lazar <104980334+jonasz-lazar-pwr@users.noreply.github.com>
2024-10-20 13:26:55 +0200

    test: Implementation of new test to check SA and TS algorithms functionality

--- tests/test_tabu_search.py
+++ tests/test_tabu_search.py
@@ -1,12 +1,17 @@
 # src/tests/test_tabu_search.py
 
+import multiprocessing
+import time
+import pynng
+
+# Importy dla algorytmu TS i plików TSP
 import compiled_binaries.TabuSearch as TS
-from src.backend.components.tsplib_management.tsp_file import TSPFile
-from src.backend.components.tsplib_management.tsplib_parser import TSPLIBParser
-from src.config import get_path
+from src.backend.components.tsp_management.tsp_file import TSPFile
+from src.backend.components.tsp_management.tsplib_parser import TSPLIBParser
+from src.utils.path_config import get_path
 
 # Ścieżki do plików
-tsp_file_path = get_path("resources/tsplib_test/berlin52.tsp")
+tsp_file_path = get_path("resources/tsplib/berlin52.tsp")
 optimal_results_path = get_path("resources/metadata/optimal_results.json")
 
 # Inicjalizacja parsera TSPLIB
@@ -20,22 +25,53 @@ tsp_file.load_distance_matrix()
 # Pobranie macierzy odległości
 distance_matrix = tsp_file.get_distance_matrix()
 
-# Sprawdzenie, czy macierz odległości została poprawnie załadowana
-if distance_matrix:
+# Funkcja, która odbiera dane przez socket NNG
+def receive_data():
+    """Receives data from the TS process through the socket."""
+    with pynng.Pair1() as sock:
+        sock.listen('tcp://127.0.0.1:6666')  # Inny port dla TS
+        while True:
+            try:
+                msg = sock.recv()
+                message = msg.decode()
+                if message == 'EOF':
+                    print("Simulation finished.")
+                    break
+                print(f"Received message: {message}")
+            except Exception as e:
+                print(f"Error receiving message: {e}")
+                break
 
 
-    # Stała kadencja
-    # Losowa kadencja z zakresem
-    ts_instance = TS.TabuSearch(tenure=100, dist_matrix=distance_matrix,
-                                move_type=TS.MoveType.OPT_2, duration_ms=5000,
-                                random_tenure_range=(50, 100), tenure_type=TS.TenureType.CONSTANT,
-                                limit_type=TS.TabuListLimitType.CUSTOM, custom_limit=50, max_neighbors=100,
-                                initial_solution_type=TS.InitialSolutionType.RANDOM)
+# Funkcja, która uruchamia algorytm sa
+def run_simulated_annealing():
+    # Inicjalizacja algorytmu Tabu Search z parametrami
+    ts_instance = TS.TabuSearch(
+        tenure=100, dist_matrix=distance_matrix, move_type=TS.MoveType.OPT_2,
+        duration_ms=3000, random_tenure_range=(50, 100), tenure_type=TS.TenureType.CONSTANT,
+        limit_type=TS.TabuListLimitType.CUSTOM, custom_limit=50, max_neighbors=100,
+        initial_solution_type=TS.InitialSolutionType.RANDOM
+    )
 
-    # Uruchomienie algorytmu TS
     ts_instance.run()
 
     # Wyświetlenie wyników
-    print(f"Optimal Solution: {tsp_file.optimal_result}")
-else:
-    print("Failed to load distance matrix.")
+    print("Optimal Solution:", tsp_file.optimal_result)
+
+
+# Funkcja główna
+if __name__ == '__main__':
+    # Uruchomienie procesu dla odbierania danych
+    ts_receiver_process = multiprocessing.Process(target=receive_data)
+    ts_receiver_process.start()
+
+    # Małe opóźnienie, aby upewnić się, że socket nasłuchuje
+    time.sleep(1)
+
+    # Uruchomienie procesu dla algorytmu sa
+    ts_process = multiprocessing.Process(target=run_simulated_annealing)
+    ts_process.start()
+
+    # Oczekiwanie na zakończenie działania obu procesów
+    ts_process.join()
+    ts_process.join()
